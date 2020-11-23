722340dc40a71deb3e325b5a96bb49114d2acefb
Manuel Rivera <smr.software@gmail.com>
2020-11-23 16:58:20 -0400

    Contratos: el spinner no se mostraba al filtrar

--- client/contratos/filtro.js
+++ client/contratos/filtro.js
@@ -15,114 +15,122 @@ import { Suscriptores } from '/imports/collections/catalogos/suscriptores';
 import { Filtros } from '/imports/collections/otros/filtros'; 
 
 angular.module("scrwebm")
-       .controller("ContratosFiltroController", ['$scope', '$state', '$stateParams', function ($scope, $state, $stateParams) {
+       .controller("ContratosFiltroController", ['$scope', '$state', '$stateParams', 
+function ($scope, $state, $stateParams) {
 
-      $scope.showProgress = false;
+    $scope.showProgress = false;
 
-      // ui-bootstrap alerts ...
-      $scope.alerts = [];
+    // ui-bootstrap alerts ...
+    $scope.alerts = [];
 
-      $scope.closeAlert = function (index) {
-          $scope.alerts.splice(index, 1);
-      };
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
 
-      $scope.origen = $stateParams.origen;
+    $scope.origen = $stateParams.origen;
 
-      // ------------------------------------------------------------------------------------------------
-      // leemos la compañía seleccionada
-      var companiaSeleccionada = CompaniaSeleccionada.findOne({ userID: Meteor.userId() });
-      if (companiaSeleccionada)
-          var companiaSeleccionadaDoc = EmpresasUsuarias.findOne(companiaSeleccionada.companiaID, { fields: { nombre: 1 } });
+    // ------------------------------------------------------------------------------------------------
+    // leemos la compañía seleccionada
+    const companiaSeleccionada = CompaniaSeleccionada.findOne({ userID: Meteor.userId() });
+    let companiaSeleccionadaDoc = null; 
 
-      $scope.companiaSeleccionada = {};
+    if (companiaSeleccionada) { 
+        companiaSeleccionadaDoc = EmpresasUsuarias.findOne(companiaSeleccionada.companiaID, { fields: { nombre: 1 } });
+    }
+        
+    $scope.companiaSeleccionada = {};
 
-      if (companiaSeleccionadaDoc)
-          $scope.companiaSeleccionada = companiaSeleccionadaDoc;
-      else
-          $scope.companiaSeleccionada.nombre = "No hay una compañía seleccionada ...";
-      // ------------------------------------------------------------------------------------------------
+    if (companiaSeleccionadaDoc) { 
+        $scope.companiaSeleccionada = companiaSeleccionadaDoc;
+    }
+    else { 
+        $scope.companiaSeleccionada.nombre = "No hay una compañía seleccionada ...";
+    }
+    // ------------------------------------------------------------------------------------------------
 
-      // -------------------------------------------------------------------------------------------
-      // leemos los catálogos en el $scope
-      $scope.tiposContrato = $scope.$meteorCollection(TiposContrato, false);
-      $scope.companias = $scope.$meteorCollection(Companias, false);
-      $scope.suscriptores = $scope.$meteorCollection(Suscriptores, false);
-      $scope.ramos = $scope.$meteorCollection(Ramos, false);
+    // -------------------------------------------------------------------------------------------
+    // leemos los catálogos en el $scope
+    $scope.tiposContrato = $scope.$meteorCollection(TiposContrato, false);
+    $scope.companias = $scope.$meteorCollection(Companias, false);
+    $scope.suscriptores = $scope.$meteorCollection(Suscriptores, false);
+    $scope.ramos = $scope.$meteorCollection(Ramos, false);
 
-      // para limpiar el filtro, simplemente inicializamos el $scope.filtro ...
-      $scope.limpiarFiltro = function () {
-          $scope.filtro = {};
-      };
+    // para limpiar el filtro, simplemente inicializamos el $scope.filtro ...
+    $scope.limpiarFiltro = function () {
+        $scope.filtro = {};
+    };
 
-      $scope.aplicarFiltroYAbrirLista = function () {
+    $scope.aplicarFiltroYAbrirLista = function () {
 
-          $scope.showProgress = true;
+        $scope.showProgress = true;
 
-          Meteor.call('contratos.leerDesdeMongo', JSON.stringify($scope.filtro), $scope.companiaSeleccionada._id, (err) => {
+        Meteor.call('contratos.leerDesdeMongo', JSON.stringify($scope.filtro), $scope.companiaSeleccionada._id, (err) => {
 
-              if (err) {
-                  const errorMessage = mensajeErrorDesdeMethod_preparar(err);
+            if (err) {
+                const errorMessage = mensajeErrorDesdeMethod_preparar(err);
 
-                  $scope.alerts.length = 0;
-                  $scope.alerts.push({
-                      type: 'danger',
-                      msg: errorMessage
-                  });
+                $scope.alerts.length = 0;
+                $scope.alerts.push({
+                    type: 'danger',
+                    msg: errorMessage
+                });
 
-                  $scope.showProgress = false;
-                  $scope.$apply();
-                  return;
-              }
+                $scope.showProgress = false;
+                $scope.$apply();
+                return;
+            }
 
-              // ------------------------------------------------------------------------------------------------------
-              // guardamos el filtro indicado por el usuario
+            // ------------------------------------------------------------------------------------------------------
+            // guardamos el filtro indicado por el usuario
 
-              if (Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() }))
-                  // el filtro existía antes; lo actualizamos
-                  // validate false: como el filtro puede ser vacío (ie: {}), simple schema no permitiría eso; por eso saltamos la validación
-                  Filtros.update(Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() })._id,
-                                 { $set: { filtro: $scope.filtro } },
-                                 { validate: false });
-              else
-                  Filtros.insert({
-                      _id: new Mongo.ObjectID()._str,
-                      userId: Meteor.userId(),
-                      nombre: 'contratos',
-                      filtro: $scope.filtro
-                  });
+            if (Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() })) { 
+                // el filtro existía antes; lo actualizamos
+                // validate false: como el filtro puede ser vacío (ie: {}), simple schema no permitiría eso; por eso saltamos la validación
+                Filtros.update(Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() })._id,
+                    { $set: { filtro: $scope.filtro } },
+                    { validate: false });
+            }  
+            else { 
+                Filtros.insert({
+                    _id: new Mongo.ObjectID()._str,
+                    userId: Meteor.userId(),
+                    nombre: 'contratos',
+                    filtro: $scope.filtro
+                });
+            }
+                
+            $scope.showProgress = false;
 
-              $scope.showProgress = false;
+            // limit es la cantidad de items en la lista; inicialmente es 50; luego avanza de 50 en 50 ...
+            $state.go('contratosLista', { origen: $scope.origen, limit: 50 });
+        })
+    }
 
-              // limit es la cantidad de items en la lista; inicialmente es 50; luego avanza de 50 en 50 ...
-              $state.go('contratosLista', { origen: $scope.origen, limit: 50 });
-          });
-      };
+    $scope.nuevo = function () {
+        $state.go('contrato.generales', {
+            origen: $scope.origen,
+            id: "0",
+            limit: 50,
+            vieneDeAfuera: false
+        });
+    }
 
-      $scope.nuevo = function () {
-          $state.go('contrato.generales', {
-              origen: $scope.origen,
-              id: "0",
-              limit: 50,
-              vieneDeAfuera: false
-          });
-      };
+    // ------------------------------------------------------------------------------------------------------
+    // si hay un filtro anterior, lo usamos
+    // los filtros (solo del usuario) se publican en forma automática cuando se inicia la aplicación
+    $scope.filtro = {};
+    const filtroAnterior = Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() });
 
-      // ------------------------------------------------------------------------------------------------------
-      // si hay un filtro anterior, lo usamos
-      // los filtros (solo del usuario) se publican en forma automática cuando se inicia la aplicación
-      $scope.filtro = {};
-      var filtroAnterior = Filtros.findOne({ nombre: 'contratos', userId: Meteor.userId() });
+    // solo hacemos el subscribe si no se ha hecho antes; el collection se mantiene a lo largo de la session del usuario
+    if (filtroAnterior) { 
+        $scope.filtro = lodash.clone(filtroAnterior.filtro);
+    }
+    // ------------------------------------------------------------------------------------------------------
 
-      // solo hacemos el subscribe si no se ha hecho antes; el collection se mantiene a lo largo de la session del usuario
-      if (filtroAnterior)
-          $scope.filtro = lodash.clone(filtroAnterior.filtro);
-      // ------------------------------------------------------------------------------------------------------
-
-      // esta tabla es necesaria para inicializar algunas partes del contrato cuando el usuario lo agrega 
-      $scope.showProgress = true;
-      Meteor.subscribe('contratosParametros', () => {
-          $scope.showProgress = false;
-          $scope.$apply();
-      });
-  }
-])
+    // esta tabla es necesaria para inicializar algunas partes del contrato cuando el usuario lo agrega 
+    $scope.showProgress = true;
+    Meteor.subscribe('contratosParametros', () => {
+        $scope.showProgress = false;
+        $scope.$apply();
+    })
+}])
